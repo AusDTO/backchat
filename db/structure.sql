36eeb32b47a85eb45c1a81db9ad946b00ec1216e
Alex Sadleir <alex.sadleir@digital.gov.au>
2018-02-20 15:29:25 +1100

    try circleci2

--- db/structure.sql
+++ db/structure.sql
@@ -1,7 +1,5 @@
 SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
+SET lock_timeout ET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
