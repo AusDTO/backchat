fa609dd6fa46208569f245e33939f96f35127d10
Alex Sadleir <alex.sadleir@digital.gov.au>
2018-02-23 16:17:54 +1100

    fix json seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -9,13 +9,13 @@ User.create!([
 #SELECT setval('users_id_seq',3);
 ActiveRecord::Base.connection.execute("SELECT setval('users_id_seq', 3);")
 Form.delete_all
-f = Form.new(name: "My Form", website: "website.com", owner_id:2, input_fields: '[{"name": "site_or_page"}, {"name": "feeling"}, {"name": "feedback"}, {"name": "referrer"}, {"name": "fp"}]')
+f = Form.new(name: "My Form", website: "website.com", owner_id:2, input_fields: [{"name": "site_or_page"}, {"name": "feeling"}, {"name": "feedback"}, {"name": "referrer"}, {"name": "fp"}])
 f.save
 Output.delete_all
-o = Output.new(name: "SlackTest",type: "SlackOutput",configuration: '{"webhook_url":"http://localhost"}', owner_id:2, form_ids: [f.id])
+o = Output.new(name: "SlackTest",type: "SlackOutput",configuration: {"webhook_url":"http://localhost"}, owner_id:2, form_ids: [f.id])
 o.save
 Submission.delete_all
-s = Submission.new(content: '{"a":"a"}',form_id: f.id)
+s = Submission.new(content: {"a":"a"},form_id: f.id)
 s.save
 OutputJob.delete_all
 oj = OutputJob.new(output_id: o.id, submission_id: s.id)
